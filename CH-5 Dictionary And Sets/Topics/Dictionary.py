a5df78afa4201fa39921877d6aeec2a41f221bfa
gitprocoder <githubforprocoders@gmail.com>
2026-05-07 17:54:44 +0530

    Python Course

---
+++ CH-5 Dictionary And Sets/Topics/Dictionary.py
@@ -0,0 +1,9 @@
+Fruits= {
+
+    "Apple":4,
+    "Banana":7,
+    "Mango":8
+}
+
+print(Fruits,type(Fruits))
+print(Fruits["Banana"])
--- Ch-4 List And Tuples/Practice Sets/Problem 3.py
+++ Ch-4 List And Tuples/Practice Sets/Problem 3.py
@@ -1,4 +1,4 @@
 A=(1,89,67,45,35)
-#If we change the tuple it will show error want to try decomment it.
+#If we change the tuple it will show error. If want to try, decomment it.
 '''A[3]=20
 print(A)'''
